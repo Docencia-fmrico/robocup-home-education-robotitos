d52df4e3308e3a0a085bd924e46437c72ce2b2dc
Juan Fernandez Eva <j.fernandezev.2018@alumnos.urjc.es>
2022-04-29 12:13:59 +0200

    find_my_mates GotoArena/Referee implementation
    Signed-off-by: Juan Fernandez Eva <j.fernandezev.2018@alumnos.urjc.es>

---
+++ carry_my_luggage/include/carry_my_luggage/GotoArena.h
@@ -0,0 +1,45 @@
+#ifndef CARRY_MY_LUGGAGE_GOTOARENA_H
+#define CARRY_MY_LUGGAGE_GOTOARENA_H
+
+#include "behaviortree_cpp_v3/behavior_tree.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+#include <move_base_msgs/MoveBaseAction.h>
+#include "carry_my_luggage/BTNavAction.h"
+#include "sensor_msgs/LaserScan.h"
+#include "ros/ros.h"
+
+#define SECURITY_DISTANCE 0.45
+#define CENTER_LASER 540
+
+namespace carry_my_luggage
+{
+
+class GotoArena : public BTNavAction
+{
+  public:
+    explicit GotoArena(const std::string& name,
+    const std::string & action_name,
+    const BT::NodeConfiguration & config);
+
+    void on_halt() override;
+    BT::NodeStatus on_tick() override;
+    void on_start() override;
+    void on_feedback(const move_base_msgs::MoveBaseFeedbackConstPtr& feedback) override;
+    void GotoArenaCallBack(const sensor_msgs::LaserScan::ConstPtr& laser);
+
+     static BT::PortsList providedPorts()
+    {
+        return{};
+    }
+
+  private:
+    ros::NodeHandle n_;
+    ros::Subscriber sub_laser_;
+    move_base_msgs::MoveBaseGoal pos_referee_;
+    bool obstacle_detected_;
+    double dist_;
+};
+
+}  // namespace carry_my_luggage
+
+#endif  // CARRY_MY_LUGGAGE_GOTOARENA_H 
