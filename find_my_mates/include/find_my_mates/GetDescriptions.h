50795b38af89a143a35cd07ef7949b4c3c83363f
Francisco Gomez Lopez <f.gomezl.2019@alumnos.urjc.es>
2022-05-05 13:20:38 +0200

    Get descriptions y cambios en cmake y package
    Signed-off-by: Francisco Gomez Lopez <f.gomezl.2019@alumnos.urjc.es>

---
+++ find_my_mates/include/find_my_mates/GetDescriptions.h
@@ -0,0 +1,41 @@
+#ifndef FIND_MY_MATES_GET_DESCRIPTIONS_H
+#define FIND_MY_MATES_GET_DESCRIPTIONS_H
+
+#include <gb_dialog/DialogInterface.h>
+#include "ros/ros.h"
+#include <string>
+
+namespace ph = std::placeholders;
+
+namespace find_my_mates
+{
+class Dialog : public gb_dialog::DialogInterface
+{
+    public:
+        Dialog();
+        std::string intent_;
+        std::string person1[3];
+        std::string person2[3];
+        std::string person3[3];
+        std::string getContext(dialogflow_ros_msgs::DialogflowResult result);
+        std::string getIntent(dialogflow_ros_msgs::DialogflowResult result);
+        void noIntentCB(dialogflow_ros_msgs::DialogflowResult result);
+        void personNameCB(dialogflow_ros_msgs::DialogflowResult result);
+        void clothesColorCB(dialogflow_ros_msgs::DialogflowResult result);
+        void personObjectCB(dialogflow_ros_msgs::DialogflowResult result);
+        void step();
+    private:
+        int state_;
+        int person_cnt_ = 1;
+        std::string context_;
+        static const int IDLE = 0;
+        static const int SPEAK = 1;
+        static const int LISTEN = 2;
+        ros::Time speak_ts_;
+        ros::NodeHandle nh_;
+};
+
+} //namespace find_my_mates
+
+
+#endif // FIND_MY_MATES_GET_DESCRIPTIONS_H
